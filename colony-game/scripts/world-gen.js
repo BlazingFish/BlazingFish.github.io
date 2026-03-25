776c3a62fd99c82cf0a203011657a01e6b4900b6
BlazingFish <henry.anastasios693@outlook.com>
2026-03-25 23:26:33 +1030

    added colouring based on ground distance

--- colony-game/scripts/world-gen.js
+++ colony-game/scripts/world-gen.js
@@ -8,8 +8,17 @@ generate()
 
 // 1D perlin noise
 
+function randomizeRGB(red, green, blue, variance) {
+    const r = (1+(Math.random()-0.5)/variance)*red
+    const g = (1+(Math.random()-0.5)/variance)*green
+    const b = (1+(Math.random()-0.5)/variance)*blue
+
+    return ("rgb("+String(r)+","+String(g)+","+String(b)+")")
+}
+
 function generate() {
     var tiles = []
+    var scale = 1
 
     width = window.innerWidth;
 	height = window.innerHeight;
@@ -17,30 +26,45 @@ function generate() {
     canvas.width = width;
 	canvas.height = height;
 
-    for (let x = 0; x < width; x++) {
+    for (let x = 0; x < width/scale; x++) {
         tiles.push([])
-        for (let y = 0; y < height; y++) {
+        for (let y = 0; y < height/scale; y++) {
             random = Math.random()
             tiles[x].push([])
 
-            var tile = false
-            if (
-                height - y < (((noise.simplex2(x/8000,0)+1)/2)*height)/4+height/8
-            ) {
-                tile = true
-            }
-
-            if (tile == true) {
-                ctx.fillRect(x, y, 1, 1);
-                tiles[x][y] = true
+            var tile
+            var ground = (((noise.simplex2(x/2000,0)+1)/2)*height/scale)/6+height/scale/8
+            if (height/scale - y < ground) {
+                if (height/scale - y > ground-10) {
+                    tile = "grass"
+                }
+                else {
+                    tile = "earth"
+                }
             }
             else {
-                tiles[x][y] = false
+                tile = "air"
             }
+
+            if (tile == "grass") {
+                ctx.fillStyle = randomizeRGB(34, 139, 34, 12);
+                ctx.fillRect(x*scale, y*scale, scale, scale);
+            }
+            else if (tile == "earth") {
+                ctx.fillStyle = randomizeRGB(120, 69, 19, 15)
+                ctx.fillRect(x*scale, y*scale, scale, scale);
+            }
+            else if (tile == "air") {
+                ctx.fillStyle = "rgb(0, 0, 0)";
+                ctx.fillRect(x*scale, y*scale, scale, scale);
+            }
+
+
+            tiles[x][y] = tile
         }
     }
 
-    console.log(tiles)
+    // console.log(tiles)
 }
 
 function getNoise(x) {
