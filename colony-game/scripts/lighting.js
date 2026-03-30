7d303cdd1b1263eba889c9f479497617f87f4b87
BlazingFish <henry.anastasios693@outlook.com>
2026-03-30 18:30:10 +1030

    made lightning but its buggy and driving me mad

--- colony-game/scripts/lighting.js
+++ colony-game/scripts/lighting.js
@@ -7,28 +7,51 @@ function checkAroundLight(lights) {
     let lightsArray = lights.split(",")
     let x = parseInt(lightsArray[0])
     let y = parseInt(lightsArray[1])
-    let intensity = parseFloat(lightsArray[2])
+    let intensity = parseFloat(lightsArray[2]) // Between 0 and 1
 
-    let radius = 50**(intensity)
+    let radius = 16 + intensity*6
 
-    var quad1 = []
+    // console.log(intensity)
 
-    for (let yQuad = 0; yQuad <= radius; yQuad++) {
-        // console.log(radius)
-        let xQuad = Math.ceil((radius**2-yQuad**2)**(1/2))
-        console.log(xQuad, yQuad)
-        quad1.push([xQuad, yQuad])
+    var circle = [] // x, y, brightness
+
+    for (let xCoord = -radius; xCoord <= radius; xCoord++) {
+        if (0 < x + xCoord < tiles.length) {
+            for (let yCoord = -radius; yCoord <= radius; yCoord++) {
+                if (0 < y + yCoord < tiles[x].length) {
+                    let distance = ((xCoord**2)+(yCoord**2))**(1/2)
+                    if (distance <= radius) {
+                        circle.push([xCoord, yCoord, 1-(distance/radius)**2])
+                    }
+                }
+            }
+        }
     }
 
-    for (let i = 0; i < quad1.length; i++){
-        // console.log("SHIT" + parseFloat(x + quad1[i][0]))
+    // console.log(circle)
 
-        tiles[x + quad1[i][0]][y + quad1[i][1]] = ["torch", 1]
-        drawTiles([tiles[x+quad1[i][0]], tiles[x+quad1[i][0]]])
+    for (let i = 0; i < circle.length; i++){
+        // if (0 < x + circle[i][0] < tiles.length && 0 < y + circle[i][1] < tiles[i].length) {
+        // console.log(tiles[i].length, y + circle[i][1])
+
+        if (tiles[x + circle[i][0], y + circle[i][1]] != undefined) {
+            // console.log(tiles[x + circle[i][0], y + circle[i][1]])
+            tiles[x + circle[i][0]][y + circle[i][1]][1] = circle[i][2]
+            drawTiles(x+circle[i][0], y+circle[i][1])
+        }
+        // tiles[x + circle[i][0]][y + circle[i][1]][1] = circle[i][2]
+        // console.log(tiles[x + circle[i][0], y + circle[i][1]])
+
+        // drawTiles(x+circle[i][0], y+circle[i][1])
+
+        // }
+        // tiles[x + circle[i][0]][y + circle[i][1]][1] = circle[i][2]
+        // drawTiles(x+circle[i][0], y+circle[i][1])
+        // console.log(tiles[x+circle[i][0]], tiles[y+circle[i][1]])
     }
 
     // console.log(radius)
     // console.log(x, y, intensity)
-    return 1
+    // return 1
 
 }
--- colony-game/scripts/world-gen.js
+++ colony-game/scripts/world-gen.js
@@ -20,8 +20,8 @@ generate()
 // }
 
 function generate() {
-    width = window.innerWidth;
-	height = window.innerHeight;
+    var width = window.innerWidth;
+	var height = window.innerHeight;
 
     canvas.width = width;
 	canvas.height = height;
@@ -74,7 +74,7 @@ function generate() {
                         }
                     }
                     else {
-                        tile = ["air"]
+                        tile = ["air", 0]
                     }
                 }
     
@@ -98,25 +98,6 @@ function generate() {
     // console.log(tiles[0].length)
 }
 
-// function colourTile(properties, scale) {
-//     if (properties[0] == "grass") {
-//         return randomizeRGB(34, 139, 34, 6);
-//     }
-//     else if (properties[0] == "earth") {
-//         return randomizeRGB(120+properties[1]*scale/2, 69+properties[1]*scale/2, 19+properties[1]*scale/2, 10)
-//         // return randomizeRGB(120, 69, 19, 10)
-//     }
-//     // else if (properties[0] == "air") {
-//     //     return "rgb(0, 0, 0)";
-//     // }
-//     else if (properties[0] == "rock") {
-//         return "rgb(100, 100, 100"
-//     }
-//     else {
-//         return false
-//     }
-//     // return
-// }
 
 function approxOne(spread) {
     return (Math.random()+0.5)/((spread-1)/2*spread)
